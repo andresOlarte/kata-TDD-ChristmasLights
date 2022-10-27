7c28e09db287a46bda2bbb74fbb30be7e35daabd
andresOlarte <deuterocanonico@gmail.com>
2022-10-27 08:23:05 -0700

    [TEST] passed

--- src/main/java/practice/christmaslights/ChristmasLights.java
+++ src/main/java/practice/christmaslights/ChristmasLights.java
@@ -2,4 +2,12 @@ package practice.christmaslights;
 
 public class ChristmasLights {
 
+	private int[][] lights = new int[1000][1000];
+
+	public int turnOn(int column, int row) {
+		lights[column][row] = 1;
+		return lights[column][row];
+	}
+	
+
 }
--- src/test/java/practice/christmaslights/ChristmasLightsTest.java
+++ src/test/java/practice/christmaslights/ChristmasLightsTest.java
@@ -1,11 +1,23 @@
 package practice.christmaslights;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
+
 
 public class ChristmasLightsTest {
 
 	@Test
 	void canCreateChristmasLights() {
 		ChristmasLights christmasLights = new ChristmasLights();
+	}
+	
+	@Test
+	void cantTurnOnASingleLight() throws Exception {
+		ChristmasLights christmasLights = new ChristmasLights();
+		assertThat(christmasLights.turnOn(0,0), is(1));
 	}
 }
